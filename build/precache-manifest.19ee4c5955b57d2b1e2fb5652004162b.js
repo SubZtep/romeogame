0df3099ba03ef0603cd5449615fcab12a5e3275e
SubZtep <subztep@gmail.com>
2019-10-02 18:35:56 +0100

    :hearts: Test build

---
+++ build/precache-manifest.19ee4c5955b57d2b1e2fb5652004162b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "637e28ff256fe127f7593c41e2df2612",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2b6c13558c2581e70da",
+    "url": "/static/js/2.59a98edc.chunk.js"
+  },
+  {
+    "revision": "d2e95199e0cbb896e829",
+    "url": "/static/js/main.441b18e9.chunk.js"
+  },
+  {
+    "revision": "c79678632407e3953b07",
+    "url": "/static/js/runtime-main.69648139.js"
+  }
+]);
